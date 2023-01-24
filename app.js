987d141285794efdf4e095194a31dc8d7a356fc9
akosy <develllopeurweb@gmail.com>
2023-01-24 14:38:45 +0100

    Ajout des exercices sur le cours objects

--- app.js
+++ app.js
@@ -3,45 +3,98 @@
 // Exercice 1 : Création et manipulation d'objets
 
 //     Etape 1 : Créer un objet "voiture" avec les propriétés "marque", "modèle" et "année"
+
 let voiture = {
-    marque: "mercedes",
-    modèle:"Classe B " ,
-    année:"2022"
+    marque: "MERCEDES",
+    modele: "CLASSE B ",
+    annee: "2022",
+
+    //     Etape 2 : Ajouter une méthode "conduire" à l'objet "voiture" qui affiche un message "La voiture de marque [marque] modèle [modèle] de l'année [année] roule sur la route."
+    conduire: function () {
+        console.log("La voiture de marque " + this.marque + " modèle " + this.modele + " de l'année " + this.annee + " roule sur la route.");
+    }
 }
-//     Etape 2 : Ajouter une méthode "conduire" à l'objet "voiture" qui affiche un message "La voiture de marque [marque] modèle [modèle] de l'année [année] roule sur la route."
+
 
 //     Etape 3 : Créer une instance de l'objet "voiture" en utilisant les propriétés "Toyota", "Camry" et "2020"
+let maVoiture = Object.create(voiture, {
+    marque: { value: "Toyota" },
+    modele: { value: "Camry" },
+    annee: { value: "2020" }
+});
+
 //     Etape 4 : Appeler la méthode "conduire" sur l'instance de l'objet "voiture" pour afficher le message "La voiture de marque Toyota modèle Camry de l'année 2020 roule sur la route."
+maVoiture.conduire();
+
 
 
 
 // Exercice 2 : Manipulation d'objets dans un tableau
 
 //     Etape 1 : Créer un tableau "garage" qui contient des instances de l'objet "voiture" créé dans l'exercice précédent
-//     Etape 2 : Ajouter une méthode "afficherVoitures" à l'objet "garage" qui affiche les propriétés de chaque voiture dans le tableau
-//     Etape 3 : Appeler la méthode "afficherVoitures" sur l'objet "garage" pour afficher les propriétés de chaque voiture dans le tableau
+let garage = [
+    Object.assign(Object.create(voiture), { marque: "Toyota", modele: "Camry", annee: "2020" }),
+    Object.assign(Object.create(voiture), { marque: "Honda", modele: "Accord", annee: "2019" }),
+    Object.assign(Object.create(voiture), { marque: "Ford", modele: "Mustang", annee: "2018" })
+];
 
+
+//     Etape 2 : Ajouter une méthode "afficherVoitures" à l'objet "garage" qui affiche les propriétés de chaque voiture dans le tableau
+garage.afficherVoitures = function () {
+    for (let i = 0; i < this.length; i++) {
+        console.log("Marque: " + this[i].marque + ", Modèle: " + this[i].modele + ", Année: " + this[i].annee);
+    }
+}
+
+//     Etape 3 : Appeler la méthode "afficherVoitures" sur l'objet "garage" pour afficher les propriétés de chaque voiture dans le tableau
+garage.afficherVoitures();
 
 
 // Exercice 3 : Fonction constructeur
 
 //     Etape 1 : Créer une fonction constructeur "Personne" qui prend en paramètres "nom" et "âge"
+function Personne(nom, age) {
 //     Etape 2 : Ajouter les propriétés "nom" et "âge" à l'objet créé par la fonction constructeur
+    this.nom = nom;
+    this.age = age;
 //     Etape 3 : Ajouter une méthode "sePresenter" à l'objet créé par la fonction constructeur qui affiche un message "Bonjour, je m'appelle [nom] et j'ai [âge] ans."
+    this.sePresenter = function () {
+        console.log("Bonjour, je m'appelle " + this.nom + " et j'ai " + this.age + " ans.");
+    }
+}
 //     Etape 4 : Utiliser la fonction constructeur pour créer une instance de l'objet "Personne" en utilisant les valeurs "John" et "30"
 
-
+let personne1 = new Personne("John", 30);
+personne1.sePresenter();
 
 // Exercice 4 : Héritage de fonction constructeur
 
 //     Etape 1 : Créer une fonction constructeur "Etudiant" qui hérite des propriétés et méthodes de la fonction constructeur "Personne"
-//     Etape 2 : Ajouter une propriété "matricule" à l'objet créé par la fonction constructeur "Etudiant"
-//     Etape 3 : Ajouter une méthode "etudier" à l'objet créé par la fonction constructeur "Etudiant" qui affiche un message "L'étudiant [nom] avec le matricule [matricule] étudie pour son examen."
+function Etudiant(nom, age, matricule) {
+    Personne.call(this, nom, age);
+
+
+    //     Etape 2 : Ajouter une propriété "matricule" à l'objet créé par la fonction constructeur "Etudiant"
+    this.matricule = matricule;
+
+
+    //     Etape 3 : Ajouter une méthode "etudier" à l'objet créé par la fonction constructeur "Etudiant" qui affiche un message "L'étudiant [nom] avec le matricule [matricule] étudie pour son examen."
+    this.etudier = function () {
+        console.log("L'étudiant " + this.nom + " avec le matricule " + this.matricule + " étudie pour son examen.");
+    }
+}
+
+
 //     Etape 4 : Utiliser la fonction constructeur "Etudiant" pour créer une instance de l'objet "Etudiant" en utilisant les valeurs "Jane" et "22" pour les propriétés "nom" et "âge", et "123456" pour la propriété "matricule"
+let etudiant1 = new Etudiant("Jane", 22, "123456");
+
+
 //     Etape 5 : Appeler la méthode "sePresenter" sur l'instance de l'objet "Etudiant" pour afficher le message "Bonjour, je m'appelle Jane et j'ai 22 ans."
+
+etudiant1.sePresenter();
+
 //     Etape 6 : Appeler la méthode "etudier" sur l'instance de l'objet "Etudiant" pour afficher le message "L'étudiant Jane avec le matricule 123456 étudie pour son examen."
-
-
+etudiant1.etudier();
 
 
 // Exercice à push sur Github :
